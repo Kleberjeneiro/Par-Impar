9379aac154cdaa16028e6a4a1a113a734daf2ea6
Kleberjeneiro <kleber.jeneirokleber.jeneiro@gmail.com>
2023-09-11 00:46:11 -0300

    Alteração do type = text para munber

---
+++ checar.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Checar se o número e par ou impar</title>
+
+    </head>
+
+    <body>
+
+        <h1></h1>
+
+        <fieldset>
+
+            <legend>Par ou impar</legend>
+            <div class="container">
+
+                <?php
+
+                        $num = $_POST["numero"];
+
+                        if ($num % 2 == 0) {
+
+                            echo "Número Par";
+
+                        }else {
+
+                            echo "Número Impar";            
+                        }
+                ?>
+                <br><br>
+                <a href="index.php"> Voltar </a>
+
+            </div>
+             
+
+        </fieldset>
+        
+    </body>
+    
+</html>
+
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Checar se o número e par ou impar</title>
+
+    </head>
+
+    <body>
+
+        <h1></h1>
+
+        <fieldset>
+
+            <legend>Par ou impar</legend>
+            <div class="container">
+
+                <form action="checar.php" method="POST">
+
+                    <label for="numero">Número</label><br>
+                    <input type="number" id="numero" name="numero" placeholder="Digite qualquer número" required>
+
+                    <input type="submit" value="Consultar">
+
+
+                </form>
+            </div>
+             
+
+        </fieldset>
+        
+    </body>
+    
+
+
+
+</html>
